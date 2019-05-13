6b08d757d2222166d5bc72a37848446b1dc0c6fc
Neville Omangi <nevomangi@gmail.com>
2019-05-14 00:07:03 +0300

    show levels

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
 const canvasWidth = 400
 const canvasHeight = 400
 
-let x, y
-let delay = 1000
+let x, y // position of circle
+let delay = 1000 // interval delay to reset circle position
 let radius = 100
 let score = 0
+let level = 1
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight)
-  x = random(radius, canvasWidth - radius)
+
+  // pick random circle position, ensure circle is not cut off
+  x = random(radius, canvasWidth - radius) 
   y = random(radius, canvasHeight - radius)
 }
 
 function draw() {
   background(251)
   
-  noStroke()
+  noStroke() // remove default black stroke
   fill(127, 0, 127)
-  ellipse(x, y, radius * 2, radius * 2)// x, y, w, h
+  ellipse(x, y, radius * 2, radius * 2) // x position, y position, width, height
   
-  text("Score: " + score, 10, 20);
+  text(`Score: ${score}`, 10, 20); // str, x, y
 }
 
 function mousePressed() {
-  let d = dist(mouseX, mouseY, x, y) 
+  let d = dist(mouseX, mouseY, x, y) // dist calculates the distance between two points. Args: x1, y1, x2, y2
   
-  if (d < 100) {
+  if (d < 100) { // if click is within circle
     score++
     relocateCircle()
     
-    if (score % 10 === 0) {
+    if (score % 10 === 0) { // upgrade level after score of 10, 20, 30 ... 
         upgradeLevel()        
     }
   }
@@ -41,9 +44,13 @@ function relocateCircle() {
 }
 
 function upgradeLevel() {
-  radius /= 1.5
-  delay *= 0.8
+  radius /= 1.5 // make circle smaller
+  delay *= 0.8 // make circle move faster
+  level += 1
   resetInterval()
+
+  // show level message
+  levelEl.innerHTML = `You are now on level ${level}`
 }
 
 function resetInterval() {
@@ -53,5 +60,10 @@ function resetInterval() {
 }
  
 let relocateInterval = setInterval(relocateCircle, delay)
+
+// 
+
+const levelEl = document.getElementById('level')
+levelEl.innerHTML = `You are now on level ${level}`
 
 // adapted from https://medium.com/@kellylougheed/make-your-first-game-with-p5-js-38bfb308a671
